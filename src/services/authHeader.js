888d2ffcef0a0b2f2a9845d1bd6f6940536cee9a
Alnaive <astolo86@gmail.com>
2023-03-13 03:00:58 +0800

    fix baseURL

--- src/services/authHeader.js
+++ src/services/authHeader.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const token = localStorage.getItem('accessToken')
 const instance = axios.create({
-    baseURL: 'https://enter-mate.vercel.app/api/',
+    baseURL: 'https://entermate-backend.vercel.app/api/',
     timeout: 1000,
     headers: {'x-access-token': token, 'Content-Type': 'application/json'}
 
